8a3e9e8b7d33d2a0e4d23b978ae56c68ea46b438
jon-str <jonstrength1@gmail.com>
2014-09-02 00:30:56 -0400

    Cleaned, still debugging

--- src/com/jonstr/iris/game/IrisComponent.java
+++ src/com/jonstr/iris/game/IrisComponent.java
@@ -7,67 +7,68 @@ package com.jonstr.iris.game;
 
 import java.awt.event.KeyEvent;
 
+import com.jonstr.iris.engine.Display;
 import com.jonstr.iris.engine.Engine;
 import com.jonstr.iris.game.IrisBlock.Tetrominoe;
 import com.jonstr.iris.rendering.Bitmap;
 
-public class IrisTesting extends Bitmap {	
+public class IrisComponent extends Bitmap {	
 	
-	private int blockSize;
-	private int blockSizeSquared;
+	private final static int blockSize = 6;
+	private final static int blockSizeSquared = blockSize * blockSize;
 	
-	private boolean lR = true;
+	private final static int gridWidth = 8;
+	private final static int gridHeight = 16;
+	private final static int gridSize = gridWidth * gridHeight;
+	private final static int gridLineColor = 0xFF0080FF;
 	
-	/* TESTING */
-	final static int gridWidth = 8;
-	final static int gridHeight = 16;
+	private Tetrominoe[] grid;
+	private IrisBlock currShape;
 	
-	private float time;
+	private int curX;
+	private int curY;
 	
-	boolean isFinishedFalling = false;
+	private int numLinesRemoved;
 	
-	int curX = 0;
-	int curY = 2;
-	
-	int numLinesRemoved = 0; //score
-	IrisBlock curPeice;
-	Tetrominoe[] grid;
-	
-	public IrisTesting() {
-		super(((6 * 6) * gridWidth) + 1, ((6 * 6) * gridHeight) + 1);
-		this.blockSize = 6;
-		this.blockSizeSquared = blockSize * blockSize;
+	public IrisComponent() {
+		super(((blockSizeSquared) * gridWidth) + 1, ((blockSizeSquared) * gridHeight) + 1);
 		
-		time = System.nanoTime();
-		
-		curPeice = new IrisBlock();
 		grid = new Tetrominoe[gridWidth * gridHeight];
+		currShape = new IrisBlock();
+		
+		curX = 0;
+		curY = 0;
+		
+		numLinesRemoved = 0;
 		
 		clearGrid();
 		newShape();
-		curY = 2;
 	}
 	
-	public void test(Engine engine) {
+	public void test(Engine engine) { /*** TODO ***/ //THIS REALLY SUCKS
+		//ADD SOME DELTA FOR THE KEY REPEAT TIME ADJUSTMENT, ALLOW
+		//RENDERER TO DRAW FIXED FRAME RATE AND REDRAW SHAPE WHEN ROTATED BUT DONT ADVANCE CLOCK
 		this.fill(0xFF000000);
-		drawGrid();
-		drawShapes();
+		drawGridLines();
+		drawFallenShapes();
+		
+		this.handleInput(Display.getKeys());
 		
 		if(canMove(curX, curY + 1)) {
-			curY++;
 			drawShape();
+			curY++;
 		} else {
 			drawShape();
-			finish(engine);
+			finishFalling(engine);
 			removeLines();
-			drawShapes();
+			drawFallenShapes();
 			newShape();
 			return;
 		}
 		
 	}
 	
-	public void removeLines() {
+	private void removeLines() {
 		for(int y = 0; y < gridHeight; ++y) {
 			if(rowUsed(y)) {
 				numLinesRemoved++;
@@ -77,31 +78,27 @@ public class IrisTesting extends Bitmap {
 		}
 	}
 	
-	private int gridSize = gridWidth * gridHeight;
-	
-	public void shiftRowDown(int row) {
+	private void shiftRowDown(int row) {
 		Tetrominoe[] tempGrid = new Tetrominoe[gridSize];
 		for(int i = 0; i < gridSize; ++i) {
 			tempGrid[i] = grid[i];
 		}
 		
 		for(int i = 0; i < gridWidth; ++i) {
-			grid[i + (row * gridWidth)] = Tetrominoe.NoShape;
+			grid[i + (row * gridWidth)] = noShape();
 		}
 		
-		int len = 0;
+		/* I REALLY DO NOT LIKE THIS */
 		for(int rw = row; rw > 0; rw--) {
-			System.arraycopy(tempGrid, (rw - 1) * gridWidth, grid, rw * gridWidth, gridWidth);
-			len += gridWidth;
-		
+			System.arraycopy(tempGrid, (rw - 1) * gridWidth, grid, rw * gridWidth, gridWidth);		
 		}
 	}
 	
-	public Tetrominoe noShape() { return Tetrominoe.NoShape; }
+	private Tetrominoe noShape() { return Tetrominoe.NoShape; }
 	
-	public boolean rowUsed(int row) {
+	private boolean rowUsed(int row) {
 		for(int x = 0; x < gridWidth; x++) {
-			if(shapeAt(x, row) == Tetrominoe.NoShape) {
+			if(shapeAt(x, row) == noShape()) {
 				return false;
 			}
 		}
@@ -109,37 +106,31 @@ public class IrisTesting extends Bitmap {
 		return true;
 	}
 	
-	public void clearRow(Tetrominoe[] grid, int row) {
-		for(int i = 0; i < gridWidth; ++i) {
-			grid[(row * gridWidth)] = Tetrominoe.NoShape;
-		}
-	}
-	
-	public void drawShape() {
+	private void drawShape() {
 		for(int i = 0; i < 4; ++i) {
-			int x = curX + curPeice.getX(i);
-			int y = curY + curPeice.getY(i);
+			int x = curX + currShape.getX(i);
+			int y = curY + currShape.getY(i);
 			if(x < 0 || x >= gridWidth || y < 0 || y >= gridHeight) {
 				continue;
 			}
-			drawBlock(x, y);
+			drawCurrShape(x, y);
 		}
 	}
 	
 	public boolean canMove(int newX, int newY) {
-		if(curPeice.getMaxY() + newY > 15) {
+		if(currShape.getMaxY() + newY > 15) {
 			return false;
 		}
 		
 		for(int i = 0; i < 4; ++i) {
-			int x = curPeice.getX(i) + newX;
-			int y = curPeice.getY(i) + newY;
+			int x = currShape.getX(i) + newX;
+			int y = currShape.getY(i) + newY;
 			
 			if(x < 0 || x >= gridWidth || y < 0 || y >= gridHeight) {
 				return false;
 			}
 			
-			if(shapeAt(x, y) != Tetrominoe.NoShape) {
+			if(shapeAt(x, y) != noShape()) {
 				return false;
 			}
 		}
@@ -147,41 +138,35 @@ public class IrisTesting extends Bitmap {
 		return true;
 	}
 	
-	public void finish(Engine engine) {
+	private void finishFalling(Engine engine) {
 		if(!canMove(curX, curY)) {
 			System.out.println("Game over!!!");
 			System.out.println("Score: " + numLinesRemoved);
 			engine.stop();
 		}
 		for(int i = 0; i < 4; ++i) {
-			int x = curPeice.getX(i) + curX;
-			int y = curPeice.getY(i) + curY;
-			setShapeAt(curPeice.getBlockShape(), x, y);
+			int x = currShape.getX(i) + curX;
+			int y = currShape.getY(i) + curY;
+			setShapeAt(currShape.getBlockShape(), x, y);
 		}
 	}
 	
-	public void drawBlock(int column, int row) {
-		drawGrid();
-		//int column = 1;
-		//int row = 0;
+	public void drawCurrShape(int column, int row) {
+		drawGridLines();
 		
 		int xPix = ((column * blockSizeSquared));
 		int yPix = ((row * blockSizeSquared) + 1);
 		
-		for(int y = yPix; y < yPix + 36 - 1; ++y) {
-			for(int x = xPix; x < xPix + 36; ++x) {
+		for(int y = yPix; y < yPix + blockSizeSquared - 1; ++y) {
+			for(int x = xPix; x < xPix + blockSizeSquared; ++x) {
 				if(!isGridLine(x, y)) {
-					this.setPixel(x, y, IrisBlock.getBlockColor(curPeice.getBlockShape()));
+					this.setPixel(x, y, IrisBlock.getBlockColor(currShape.getBlockShape()));
 				}
 			}
 		}
 	}
 	
-	public void paintGrid() {
-		drawGrid();
-	}
-	
-	public void drawShapes() {
+	public void drawFallenShapes() {
 		for(int y = 0; y < this.getHeight(); y++) {
 			for(int x = 0; x < this.getWidth(); x++) {
 				if(!isGridLine(x, y)) {
@@ -190,7 +175,7 @@ public class IrisTesting extends Bitmap {
 					
 					Tetrominoe shape = shapeAt(xx, yy);
 					
-					if(shape != Tetrominoe.NoShape){
+					if(shape != noShape()){
 						this.setPixel(x, y, IrisBlock.getBlockColor(shape));
 					} else {
 						this.setPixel(x, y, 0xFF000000);
@@ -201,40 +186,35 @@ public class IrisTesting extends Bitmap {
 	}
 	
 	public void newShape() {
-		curPeice.setRandomBlockShape();
+		currShape.setRandomBlockShape();
 		curX = gridWidth / 2 - 1;
 		curY = 0;
-		isFinishedFalling = false;
 	}
 	
 	public Tetrominoe shapeAt(int x, int y) { return grid[(y * gridWidth) + x]; }
 	public void setShapeAt(Tetrominoe shape, int x, int y) { grid[(y * gridWidth) + x] = shape; }
 	
 	public void update(Engine engine) {
-		//paintGrid();
-		double prevTime = time;
-		double currTime = System.nanoTime() - prevTime;
-		
 		test(engine);
 	}
 	
-	public void clearGrid() { 
+	private void clearGrid() { 
 		for(int i = 0; i < gridWidth * gridHeight; ++i) {
-			grid[i] = Tetrominoe.NoShape;
+			grid[i] = noShape();
 		}
 	}
-	/*END TESTING SHIT*/
 	
-	public void drawGrid() {
+	private void drawGridLines() {
 		for(int y = 0; y < this.getHeight(); y++) {
 			for(int x = 0; x < this.getWidth(); x++) {
 				if(isGridLine(x, y)) {
-					this.setPixel(x, y, 0xFF0080FF);
+					this.setPixel(x, y, gridLineColor);
 				}
 			}
 		}
 	}
-	public boolean isGridLine(int x, int y) {
+	
+	private boolean isGridLine(int x, int y) {
 		if(x % (blockSize * blockSize) == 0 || y % (blockSize * blockSize) == 0) return true;
 
 		return false;
@@ -261,16 +241,7 @@ public class IrisTesting extends Bitmap {
 			return false;
 			
 		} else if(keys[KeyEvent.VK_SPACE] || keys[KeyEvent.VK_UP]) {
-			
-			if(lR) {
-				curPeice.testRot(true);
-				lR = false;
-			} else {
-				curPeice.testRot();
-				lR = true;
-			}
-			
-			//curPeice.testRot();
+			currShape.testRot(curX, curY);
 			
 			return true;
 			
