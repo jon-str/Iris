19caa81c9c2a0e4b3369d58dac30481a191f8681
jon-str <jonstrength1@gmail.com>
2014-09-09 16:12:43 -0400

    Playing with image loading

--- src/com/jonstr/iris/engine/Engine.java
+++ src/com/jonstr/iris/engine/Engine.java
@@ -1,5 +1,6 @@
 package com.jonstr.iris.engine;
 
+import com.jonstr.iris.game.IrisFontComponent;
 import com.jonstr.iris.game.IrisMainComponent;
 import com.jonstr.iris.game.IrisNextShapeComponent;
 import com.jonstr.iris.rendering.Bitmap;
@@ -17,6 +18,9 @@ public class Engine implements Runnable {
 	
 	IrisMainComponent irisGame;
 	IrisNextShapeComponent irisNextShapeGrid;
+	
+	/* WORKS */
+	//Bitmap irc_img = new Bitmap("res/bricks.jpg");
 
 	public Engine(int window_width, int window_height, String window_title) {
 		this.display = new Display(window_width, window_height, window_title);
@@ -26,6 +30,8 @@ public class Engine implements Runnable {
 		
 		irisGame = new IrisMainComponent(6, 8, 16, 0xFFFF00FF);
 		irisNextShapeGrid = new IrisNextShapeComponent(irisGame, 6, 5, 5, 0xFFFF00FF);
+		
+		System.out.println(System.getProperty("user.dir"));
 	}
 
 	public synchronized void start() {
@@ -47,7 +53,7 @@ public class Engine implements Runnable {
 	
 	@Override
 	public void run() {
-//		int frames = 0;
+		int frames = 0;
 		long unprocFrames = 0;
 		
 		dropFlag = false;
@@ -58,20 +64,19 @@ public class Engine implements Runnable {
 			long startTime = System.nanoTime();
 			
 			update();
-			//update();
 			render();
 			
 			long endTime = System.nanoTime();
 			long frameTime = endTime - startTime;
 			unprocFrames += frameTime;
 			if(unprocFrames >= 1000000000L) {
-				//System.out.println(unprocFrames + " nanos, " + frames + " fps?");
+				System.out.println(unprocFrames + " nanos, " + frames + " fps?");
 				dropFlag = true;
 				unprocFrames = 0;
-//				frames = 0;
+				frames = 0;
 			} else {
 				dropFlag = false;
-//				frames++;
+				frames++;
 			}
 		}
 	}
@@ -94,6 +99,7 @@ public class Engine implements Runnable {
 			frameBuffer.fill(0xFF000000);
 			frameBuffer.blit(irisGame, 8, 8);
 			frameBuffer.blit(irisNextShapeGrid, 8 + (6 * (6 * 8)) + 8, 8 + (6 * (6 * 11)));
+//			frameBuffer.blit(irc_img, 0, 0);
 		}
 		display.swapBuffers();
 	}
