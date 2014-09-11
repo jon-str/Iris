7adf14c91b6a3685c5d28fe3c70f5a708398c6aa
jon-str <jonstrength1@gmail.com>
2014-09-11 02:06:21 -0400

    Encapsulate game components and host draw list for rendering prioritization

---
+++ src/com/jonstr/iris/game/Game.java
@@ -0,0 +1,50 @@
+package com.jonstr.iris.game;
+
+import com.jonstr.iris.engine.Engine;
+import com.jonstr.iris.rendering.Bitmap;
+
+public class Game {
+	
+	private boolean dropFlag;
+	
+	/* VISUAL LAYOUT SCRATCH
+		xPadding = 8 + (6 * (6 * 8)) + 8;
+		yPadding = 8 + (6 * (6 * 11));
+	*/
+	
+	private IrisMainComponent irisGame;
+	private IrisNextShapeComponent irisNextShapeGrid;
+	private IrisScoreComponent irisScore;
+	
+	private GridComponent[] drawList;
+	
+	public Game() {
+		dropFlag = false;
+		irisGame = new IrisMainComponent(6, 8, 16, 0xFFFF00FF, 8, 8);
+		irisNextShapeGrid = new IrisNextShapeComponent(irisGame, irisGame.getBlockSize(), 5, 5, 
+				                                       irisGame.getGridLineColor(), 304, 404);
+		irisScore = new IrisScoreComponent(400, 300);
+			
+		drawList = new GridComponent[3];
+		drawList[0] = irisGame;
+		drawList[1] = irisNextShapeGrid;
+		drawList[2] = irisScore;
+	}
+	
+	public void update(Engine engine, boolean[] keys) {
+		irisGame.update(engine, false, irisGame.handleInput(keys)); // ENCAP
+		irisGame.update(engine, dropFlag, false); // ENCAP
+	}
+	
+	public void render() {
+		for(int i = 0; i < drawList.length; ++i) {
+			drawList[i].render();
+		}
+	}
+	
+	public void setDropFlag(boolean flag) { dropFlag = flag; }
+	
+	public GridComponent[] getDrawList() { return drawList; }
+	public GridComponent getDrawListAt(int i) { return drawList[i]; }
+	public Bitmap getBitmapToDraw(int i) { return (Bitmap) drawList[i]; }
+}
