ab38d8c29fb7f99d86c46fa676e5dff3d369e9b5
jon-str <jonstrength1@gmail.com>
2014-09-11 02:03:33 -0400

    Encapsulated game elements

--- src/com/jonstr/iris/engine/Engine.java
+++ src/com/jonstr/iris/engine/Engine.java
@@ -1,36 +1,27 @@
 package com.jonstr.iris.engine;
 
-import com.jonstr.iris.game.IrisMainComponent;
-import com.jonstr.iris.game.IrisNextShapeComponent;
+import com.jonstr.iris.game.Game;
 import com.jonstr.iris.rendering.Bitmap;
 
 public class Engine implements Runnable {
+	
 	public static InputHandler input;
-	
+
 	private boolean running;
-	
-	private boolean dropFlag;
-	
+
 	private Thread thread;
 	private Display display;
 	private Bitmap frameBuffer;
-	
-	IrisMainComponent irisGame;
-	IrisNextShapeComponent irisNextShapeGrid;
-	
-	/* WORKS */
-	//Bitmap bmp_img = Bitmap.loadBitmap("res/bricks.jpg");
-	/* OR */
-	//Bitmap bmp_img = new Bitmap("res/bricks.jpg");
+
+	private Game game;
 
 	public Engine(int window_width, int window_height, String window_title) {
 		display = new Display(window_width, window_height, window_title);
 		input = new InputHandler();
 		display.addKeyListener(input);
 		frameBuffer = display.getFrameBuffer();
-		
-		irisGame = new IrisMainComponent(6, 8, 16, 0xFFFF00FF);
-		irisNextShapeGrid = new IrisNextShapeComponent(irisGame, irisGame.getBlockSize(), 5, 5, irisGame.getGridLineColor());
+
+		game = new Game();
 	}
 
 	public synchronized void start() {
@@ -49,67 +40,53 @@ public class Engine implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run() {
-//		int frames = 0;
-		long unprocFrames = 0;
-		
-		dropFlag = false; // ENCAP
-		
 		display.requestFocus();
 		
-		while(isRunning()) {
+		// int frames = 0;
+		long unprocFrames = 0;
+
+		while (running) {
 			long startTime = System.nanoTime();
-			
+
 			update();
 			render();
-			
+
 			long endTime = System.nanoTime();
 			long frameTime = endTime - startTime;
 			unprocFrames += frameTime;
-			if(unprocFrames >= 1000000000L) { // ENCAP (GAME SPEED FOR LEVELS)
-				//System.out.println(unprocFrames + " nanos, " + frames + " fps");
-				
-				dropFlag = true; // ENCAP
+			if (unprocFrames >= 1000000000L) { // ENCAP (GAME SPEED FOR LEVELS)
+				// System.out.println(unprocFrames + " nanos, " + frames + " fps");
+
+				game.setDropFlag(true);
+				// frames = 0;
 				unprocFrames = 0;
-//				frames = 0;
 			} else {
-				dropFlag = false; // ENCAP
-//				frames++;
+				game.setDropFlag(false);
+				// frames++;
 			}
 		}
 	}
-	
-	
-	
-	private void update() {
-		irisGame.update(this, false, irisGame.handleInput(getKeys())); // ENCAP
-		irisGame.update(this, dropFlag, false); // ENCAP
-	}
-	
-	// ENCAP
-	int xPadding = 8 + (6 * (6 * 8)) + 8;
-	int yPadding = 8 + (6 * (6 * 11));
+
+	private void update() { game.update(this, input.keys); }
 	
 	private void render() {
-		if (!isRunning()) return;
-		
-		irisGame.render(); // ENCAP
-		irisNextShapeGrid.render(); // ENCAP
-		
+		if (!running) return;
+
+		game.render();
+
 		display.clear();
 		{
 			frameBuffer.fill(0xFF000000);
-			frameBuffer.blit(irisGame, 8, 8);
-			frameBuffer.blit(irisNextShapeGrid, xPadding, yPadding);
-			
-			//frameBuffer.blit(bmp_img, 0, 0);
+			for (int i = 0; i < game.getDrawList().length; ++i) {
+				frameBuffer.blit(game.getBitmapToDraw(i), 
+								 game.getDrawListAt(i).getXAbsolute(), 
+								 game.getDrawListAt(i).getYAbsolute());
+			}
 		}
 		display.swapBuffers();
 	}
 
-	public boolean isRunning() { return running; }
-	
-	public static boolean[] getKeys() { return input.keys; }
 }
